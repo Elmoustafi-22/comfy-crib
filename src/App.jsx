a8c66156e91d01e81345d8ccb224cb1abff1e48a
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-04-19 19:55:20 +0100

    created the Header UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/sign-in" element={<SignIn />}/>
-      <Route path="/sign-out" element={<SignOut />}/>
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile/>} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-cyan-100 border-b-2 border-b-cyan-200/50 shadow-md">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
+        {/* Logo  */}
+        <Link to="/">
+          <h1 className="font-lato font-extrabold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-cyan-400">Comfy</span>
+            <span className="text-cyan-800">Crib</span>
+          </h1>
+        </Link>
+
+        {/* Search Bar  */}
+        <form className="w-60 lg:w-120 px-2 py-2 border border-cyan-400 bg-white rounded-4xl items-center relative">
+          <input
+            className=" bg-transparent w-full focus:ring-0 focus:outline-0 relative"
+            type="text"
+            placeholder="Find your perfect home..."
+          />
+          <FaSearch className="text-slate-600 absolute right-3 top-3 size-4" />
+        </form>
+
+        {/* Nav links  */}
+        <ul className="flex gap-4 items-center">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 py-1 px-1 relative group">
+              Home
+              <span className="absolute bottom-0 right-0 w-0 h-0.5 bg-cyan-400 transition-all group-hover:w-full group-hover:right-auto group-hover:left-0 duration-300"></span>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 py-1 px-1 relative group">
+              About
+              <span className="absolute bottom-0 right-0 w-0 h-0.5 bg-cyan-400 transition-all group-hover:w-full group-hover:right-auto group-hover:left-0 duration-300"></span>
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-700 py-1 px-1 relative group">
+              Sign in
+              <span className="absolute bottom-0 right-0 w-0 h-0.5 bg-cyan-400 transition-all group-hover:w-full group-hover:right-auto group-hover:left-0 duration-300"></span>
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
