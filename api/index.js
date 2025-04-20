5607e6637e0340b424139410e9329fe634ad9125
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-04-20 08:12:06 +0100

    connected to the mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -9,4 +10,13 @@ app.use(express.json())
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`The localhost is live on: http://localhost:${PORT}`)
+})
+
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: 'comfy-crib-db',
+    bufferCommands: true
+}).then(() => {
+    console.log('Connected to MongoDB!')
+}).catch((error) => {
+    console.error(error)
 })
