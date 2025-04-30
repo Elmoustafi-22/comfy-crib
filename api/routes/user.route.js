cd94a643187f206291817b5ee411578d62345fcd
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-04-30 20:07:34 +0100

    added the delete user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { uploadAvatar, updateUser } from "../controllers/user.controller.js";
+import {
+  uploadAvatar,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router()
 
  router.put("/upload-avatar/:id", verifyToken, uploadAvatar);
- router.put('/update/:id', verifyToken, updateUser)
+ router.put('/update/:id', verifyToken, updateUser);
+ router.delete("/delete/:id", verifyToken, deleteUser);
 
 
  export default router
