28608f425e775596c61500c20e880cecc3488ca2
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2025-05-03 19:44:33 +0100

    add create listing api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 
@@ -23,6 +24,7 @@ app.listen(PORT, () => {
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter);
 
 mongoose.connect(process.env.MONGO_URI, {
     dbName: 'comfy-crib-db',
--- client/src/components/ProfileForm.jsx
+++ client/src/components/ProfileForm.jsx
@@ -7,7 +7,6 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
-  signOut,
   deleteUserSuccess,
   deleteUserFailure,
   signOutFailure,
